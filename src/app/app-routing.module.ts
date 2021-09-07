4904b23f32638889a45a5b3ace06cd3a8c5c989d
IvanaaVasic <ivana.p.vasic@gmail.com>
2021-09-07 23:42:50 +0200

    Login html,css i formsModule otklanjanje errora

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent}
+
 ];
 
 @NgModule({

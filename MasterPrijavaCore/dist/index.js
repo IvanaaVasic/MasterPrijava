61663183a12a09dddb5868368b750d96aab5872c
IvanaaVasic <ivana.p.vasic@gmail.com>
2021-09-14 17:22:45 +0200

    dodati cors, bodyparser, mysql

--- MasterPrijavaCore/dist/index.js
+++ MasterPrijavaCore/dist/index.js
@@ -4,12 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const mysql_1 = __importDefault(require("mysql"));
 const app = express_1.default();
+app.use(cors_1.default());
+app.use(body_parser_1.default.json());
 app.listen(4000, err => {
     if (err) {
         return console.error(err);
     }
     return console.log("App is running...");
+});
+const connection = mysql_1.default.createConnection({
+    host: "127.0.0.1",
+    user: "icka",
+    password: "pass",
+    database: "masterprijava"
 });
 app.get('/', (req, res) => {
     res.send("Zdravo!");
--- MasterPrijavaCore/src/index.ts
+++ MasterPrijavaCore/src/index.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mysql from 'mysql'
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
 app.listen(4000, err=>{
     if(err){
         return console.error(err);
@@ -8,6 +15,14 @@ app.listen(4000, err=>{
     return console.log("App is running...")
 });
 
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "ickaa",
+    password: "pass",
+    database: "masterprijava"
+}) ;
+
+connection.conect();
 
 app.get('/',(req, res) =>{
     res.send("Zdravo!")
